20cfac492d948a8646bf64c8cf46d69701e70320
Kutluhan Duman <kutluduman@gmail.com>
2020-07-28 19:29:30 +0000

    Added timer function for setting up an alarm with beep

---
+++ timer1.js
@@ -0,0 +1,11 @@
+const args = process.argv.slice(2)
+.filter((time) =>time >= 0)
+.filter((time) => !isNaN(time));
+
+args.forEach(timer => {
+  
+  setTimeout(() => {
+  process.stdout.write('\x07');
+  },timer * 1000);
+
+});
